f30a1c47ff90fda0ea68e025305f45fb74462444
Jiratchaya <jinnie0728@gmail.com>
2021-02-12 22:06:21 +0700

    initial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="Rectangle-4">
+        <h1 className="Rectangle-Copy" />
+        <img src="/img/engi.png"
+          srcSet="/img/engi@2x.png 2x,
+             /img/engi@3x.png 3x"
+          className="engi"></img>
+      </div>
+
     </div>
   );
 }
