1ddac2ce0342ec331ca10a742522d09a990d5b44
Jiratchaya <jinnie0728@gmail.com>
2021-02-12 22:49:16 +0700

    grid

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
 import './App.css';
+import Header from './component/Header';
 
 function App() {
   return (
     <div>
-      <div className="Rectangle-4">
-        <h1 className="Rectangle-Copy" />
-        <img src="/img/engi.png"
-          srcSet="/img/engi@2x.png 2x,
-             /img/engi@3x.png 3x"
-          className="engi"></img>
-      </div>
+      <Header/>
 
     </div>
   );
---
+++ src/component/Header.js
@@ -0,0 +1,18 @@
+import './Header.css'
+function Header() {
+    return (
+        <div className="header.grid">
+            <img src="img/engi.png"
+                srcset="img/engi@2x.png 2x,
+             img/engi@3x.png 3x"
+                class="engi"></img>
+            <div>
+                <div className="faculty">คณะวิศวกรรมศาสตร์</div>
+                <div className="major">สาขาวิศวกรรมศาสตร์ทั่วไป </div>  
+                <div className="university">จุฬาลงกรณ์มหาวิทยาลัย</div>
+            </div>
+        </div>
+
+    )
+}
+export default Header;
